7d9a91f67468fc5f7a6f3f39fecca82f558a60fa
andrew <andrew@pegasus>
2015-10-25 18:31:00 -0400

    initial setup and trimming of mla example project.

---
+++ pic24/sensorNode.X/build/pic24hj128gp502_89xa/production/_ext/1512199843/eeprom.o.d
@@ -0,0 +1,13 @@
+build/pic24hj128gp502_89xa/production/_ext/1512199843/eeprom.o:  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/eeprom.c  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/system.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/spi.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/console.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/system_config.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/miwi_config.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/miwi_config_p2p.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/config_89xa.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/symbol.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/timer.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/lcd.h  \
+ ../src/system_config/exp16_pic24f128ga010_89xa/delay.h 
